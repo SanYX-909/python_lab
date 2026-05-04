f946b169bc638960e32b28ab93be2c7a63a7c1b2
Alex <kaa.240703@gmail.com>
2026-05-05 00:38:01 +0300

    Завершил задачу 5: реализовал фильтрацию и вывод узких дверей

--- task_5_ifc_doors.py
+++ task_5_ifc_doors.py
@@ -1,29 +1,5 @@
 import ifcopenshell
 
-
-file_path = r"C:\Users\66910\ifc\Example_1.ifc"
-
-model = ifcopenshell.open(file_path)
-
-doors = model.by_type("IfcDoor")
-
-min_width = 0   # задать значение которые позволит разделить двери на узкие и нормальные
-
-narrow_doors = []
-
-for door in doors:
-    name = door.Name
-    width = getattr(door, "OverallWidth", None)
-    heigth = getattr(door, "OverallHeight", None)
-
-    # добавить условие если ширина меньше минимальной - добавить в список узких дверей
-
-    print("Дверь:", name, "Ширина", round(width), "Высота", heigth)
-
-# код для построчного вывода узких дверей, в таком же формате, как и нормальных
-
-import ifcopenshell
-
 file_path = r"C:\Users\66910\ifc\Example_1.ifc"
 
 model = ifcopenshell.open(file_path)
