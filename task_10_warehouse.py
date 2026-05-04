8c976d5388609b507931e3b68f8f9feba9ed0675
Alex <kaa.240703@gmail.com>
2026-05-04 22:05:20 +0300

    Создал структуру склада и вывод данных

---
+++ task_10_warehouse.py
@@ -0,0 +1,13 @@
+# Задача 10: Склад
+
+warehouse = {
+    "Кирпич": {"quantity": 5000, "price": 12.5, "min_quantity": 1000},
+    "Цемент": {"quantity": 120, "price": 450, "min_quantity": 50},
+    "Песок": {"quantity": 8, "price": 800, "min_quantity": 10},
+    "Арматура": {"quantity": 30, "price": 48000, "min_quantity": 20},
+    "Бетон": {"quantity": 45, "price": 4200, "min_quantity": 15}
+}
+
+print("Склад:")
+for name, data in warehouse.items():
+    print(name, "-", data)
