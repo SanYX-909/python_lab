7c7dfcdf9a6aebc85f83d66813c04e6f270c2f4e
Alex <kaa.240703@gmail.com>
2026-05-04 21:14:31 +0300

    Добавил нормальный код задачи 1

--- task_01_passport.py
+++ task_01_passport.py
@@ -1,1 +1,31 @@
 print("Hello")
+# Задача 1: Паспорт объекта
+
+# Переменные
+student_name = "Алекс"
+group_number = "ИС-22-1"
+project_name = "ЖК Солнечный"
+floors = 9
+height = 27.0
+is_residential = True
+construction_year = 2023
+
+# Вывод
+print("=== ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ===")
+print("Составитель:", student_name)
+print("Группа:", group_number)
+print()
+print("Объект:", project_name)
+print("Этажность:", floors, "этажей")
+print("Высота:", height, "м")
+
+if is_residential:
+    print("Тип: Жилой")
+else:
+    print("Тип: Нежилой")
+
+print("Год постройки:", construction_year)
+
+# Комментарий:
+# Объект находится в вымышленном районе.
+# Выбран для примера.
