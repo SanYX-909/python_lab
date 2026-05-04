4e1baea964741a15324404ffd6e828e4d89b78ef
Alex <kaa.240703@gmail.com>
2026-05-05 00:20:24 +0300

    Добавил подсчет стен, дверей и окон в IFC модели

--- task_4_ifc_analysis.py
+++ task_4_ifc_analysis.py
@@ -15,3 +15,11 @@ print("Схема IFC:", model.schema)
 print("Этажей:", len(storeys))
 
 # дополнить код, распечатав значения количества стен, дверей, окон
+
+walls = model.by_type("IfcWall")
+doors = model.by_type("IfcDoor")
+windows = model.by_type("IfcWindow")
+
+print("Стен:", len(walls))
+print("Дверей:", len(doors))
+print("Окон:", len(windows))
